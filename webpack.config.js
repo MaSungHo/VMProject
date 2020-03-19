5a10e4de27b641572b3f7fa0539c4315d6ecd9e9
MaSungHo <52434222+MaSungHo@users.noreply.github.com>
2020-03-19 23:02:19 +0900

    Webpack 설정 파일 구성 (#2)

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require("path");
+
+module.exports = {
+	context: path.resolve(__dirname, 'src/main/js'),
+	entry: {
+		page1: 'page1.js',
+		page2: 'page2.js'
+	},
+	devtool: 'sourcemaps',
+	cache: true,
+	output: {
+		path: __dirname,
+		filename: './src/main/webapp/bundle/[name].bundle.js'
+	},
+	mode: 'none',
+	module: {
+		rules: [ 
+			{
+				test: /\.js?$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [ '@babel/preset-env', '@babel/preset-react' ]
+					}
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [ 'style-loader', 'css-loader' ]
+			}
+		]
+	}
+};
