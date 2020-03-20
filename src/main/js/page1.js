5d731b84906182e008c1cbea99a139c213a31aea
tonem123 <tonem123@naver.com>
2020-03-20 15:48:27 +0900

    여러 자료를 반복 구문을 통해 출력

--- src/main/js/page1.js
+++ src/main/js/page1.js
@@ -4,23 +4,44 @@ import Users from '../component/Users'
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const users = {
+const users = [
+	{
 		'ID': 'mash809',
 		'PW': 'pw1234',
 		'group': '인턴',
 		'VM': 'Windows 10'
-}
-
+	},
+	{
+		'ID': 'tonem123',
+		'PW': 'msh89',
+		'group': '회사원',
+		'VM': 'Ubuntu 18.04'
+	},
+	{
+		'ID': 'mash809',
+		'PW': '08sh09',
+		'group': '학생',
+		'VM': 'CentOS 8'
+	},
+	{
+		'ID': 'mash809',
+		'PW': 'mam!!',
+		'group': '상업용',
+		'VM': 'Mac OS'
+	}
+]
 
 class Page1 extends React.Component {
 	render() {
 		return (
-			<Users
-			ID={users.ID}
-			PW={users.PW}
-			group={users.group}
-			VM={users.VM}
-			/>
+			<div>
+				{users.map(c=>{
+					return (<div>
+								<Users key={c.ID} ID={c.ID} PW={c.PW} group={c.group} VM={c.VM} />
+							    ------------------------------------------------------------------
+							</div>)
+				})}
+			</div>
 		);
 	}
 }
--- src/main/js/page2.js
+++ src/main/js/page2.js
@@ -4,19 +4,37 @@ import Groups from '../component/Groups';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const groups = {
+const groups = [
+	{
 		'name': '인턴',
 		'num_people': 12
-}
+	},
+	{
+		'name': '회사원',
+		'num_people': 5
+	},
+	{
+		'name': '학생',
+		'num_people': 31
+	},
+	{
+		'name': '상업용',
+		'num_people': 9
+	},
+]
 
 class Page2 extends React.Component {
 	render() {
 		return (
-			<Groups
-			name={groups.name}
-			num_people={groups.num_people}
-			/>
-		);
+				<div>
+					{groups.map(c=>{
+						return (<div>
+									<Groups key={c.name} name={c.name} num_people={c.num_people} />
+								    ------------------------------------------------------------------
+								</div>)
+					})}
+				</div>
+			);
 	}
 }
 
