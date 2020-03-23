c664b967b2b2dc8a558985f66b2362c491481fda
tonem123 <tonem123@naver.com>
2020-03-23 23:44:07 +0900

    홈 화면 content 중앙 정렬

--- src/main/js/home.js
+++ src/main/js/home.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import '../webapp/css/custom.css';
 
 class Home extends Component {
 	render() {
 		return (
-			<div>
-				VM 관리 웹 시스템 <br/>
-				ID: <input type="text" name="id" /> <br/>
-				Password: <input type="password" />
+			<div class="home">
+				<span class="content">
+					VM 관리 웹 시스템 <br/><br/>
+					ID: <input type="text" name="id" /> <br/><br/>
+					Password: <input type="password" />
+				</span>
+				<span class="blank"></span>
 			</div>
 		);
 	}
