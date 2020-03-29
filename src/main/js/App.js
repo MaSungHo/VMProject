d464207f88b5407d9ca38dcbd69d9b784252e42b
MaSungHo <52434222+MaSungHo@users.noreply.github.com>
2020-03-30 00:18:06 +0900

    BrowserRouter를 HashRouter로 변경, 초기화면을 로그인 화면으로 바꿈 (#12)

--- src/main/js/App.js
+++ src/main/js/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import ReactDOM from 'react-dom';
 import { Helmet } from 'react-helmet';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Menus from '../component/Menus';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import Appbar from '../component/Appbar';
 import UserList from './UserList';
 import GroupList from './GroupList';
 import Login from './Login';
@@ -13,16 +13,16 @@ class App extends Component {
 		return (
 			<div>
 			  <Helmet>
-			    <title>VM Admin Web</title>
-			  </Helmet>
+			    <title>VM Admin Project</title>
+		      </Helmet>
 			  <Router>
-			   <Menus>
+			   <Appbar>
 			     <div>
-			       <Route exact path ="/login" component={Login}/>
+			       <Route exact path ="/" component={Login} />
 			       <Route exact path ="/users" component={UserList}/>
 			       <Route exact path ="/groups" component={GroupList}/>
 			     </div>
-			   </Menus>
+			   </Appbar>
 			  </Router>
 			</div>
 		);
